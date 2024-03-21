700690fa01b315b5ccc23d2de1854ed1292a20ff
Luis Felipe Ribeiro <44618496+felipert2@users.noreply.github.com>
2024-03-21 11:05:07 -0300

    s

---
+++ Aula06 - Ciclos e Iteracoes/aula06.js
@@ -0,0 +1,7 @@
+let nome = prompt('Digite um nome: (Para sair, digite SAIR)')
+
+while (nome != 'SAIR'){
+    console.log(`O Nome digitado foi ${nome}`)
+
+    nome = prompt ('Digite um nome: (Para sair, digite SAIR)')
+}
